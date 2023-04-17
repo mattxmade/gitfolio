998df9d62b444dd18e2966d05b85c416a4fce63e
Matt <97047080+mattxmade@users.noreply.github.com>
2023-04-17 18:04:52 +0100

    Add Group component; handle props, compose elements

---
+++ src/components/Group.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+
+import Project from "./Project";
+
+const Group = (props) => {
+  return (
+    <Fragment>
+      <h2 id={props.id}>
+        <a href={`#${props.id}`}>{props.heading}</a>
+      </h2>
+
+      <ul className="projects">
+        {props.projects.map((project, index) => (
+          <li key={index} className="project-card">
+            <Project project={project} repo={props.projectsData} />
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default Group;
