e90887f8c11cc5c0ce4c086afe6d83d8888c19de
Matt <97047080+mattxmade@users.noreply.github.com>
2023-04-23 17:00:49 +0100

    Create functions; filter repository contents; log contents

--- api/services/github/github.js
+++ api/services/github/github.js
@@ -18,12 +18,31 @@ const requestConfig = {
   repo: ownerConfig.repo,
 };
 
+const { ignoreList } = ownerConfig;
+
+// Filter dir(s)/file(s) that are not required | examples >> dir: media, assets | extenstions: .jpg, .webp
+const contentFilter = (array) =>
+  array.filter((item) => {
+    if (ignoreList.directories.includes(item.name)) return;
+    if (ignoreList.extensions.some((ext) => item.name.includes(ext))) return;
+
+    return item;
+  });
+
+// Unpack repository | process content
+const unpackRepoContent = (arrayToUnpack) =>
+  contentFilter(arrayToUnpack).map((submodule) => submodule);
+
 const handleRepoContentRequest = async (req, res, next) => {
   let response = { status: "string", message: "string" };
 
   try {
     // Make a request to GitHub API | GET repository content
     const repoContent = await octokit.rest.repos.getContent(requestConfig);
+
+    // Unpack and process data
+    const contents = unpackRepoContent(repoContent.data);
+    console.log(contents);
 
     // Returns 200 | Set success message
     response.status = repoContent.status;
