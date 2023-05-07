62e57b1bf492fb32e734a9086132b94f23627514
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-07 23:45:55 +0100

    Add fontawesome CDN link tag to body [ workaround ]

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          key="icons"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <div id="root">{children}</div>
+      </body>
     </html>
   );
 }
+
+/**
+ * =============================================================================
+ * -----------------------------------------------------------------------------
+ * TODO: Grab required icons as SVGs and remove link tag
+ * -----------------------------------------------------------------------------
+ *
+ * Issue: Link tags for CDNs -- next Head is depreciated
+ * Workaround : Add link to body [ stylesheet is "body ok"]
+ *
+ * SOURCE [ "body ok" ]
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link
+ *
+ *
+ * Others with similar isssue:
+ * https://github.com/vercel/next.js/issues/46785
+
+ * =============================================================================
+ */
