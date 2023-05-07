5982216597aaf77613b256fac75c7f2823fc80a4
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-07 18:21:56 +0100

    Move components + ui tree to app directory; rename App

---
+++ app/Layout.tsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+export const metadata = {
+  title: "Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
