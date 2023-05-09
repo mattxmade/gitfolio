7c8a2bbacc289f2c09e194337925aff59fed9be1
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-09 13:49:49 +0100

    Create modal state; setup Context Prodvider; add type safety

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,81 +1,85 @@
 "use client";
 
-export const revalidate = 0;
-
 import React, { useState, useRef } from "react";
 
 import Modal from "./components/Modal";
 import Group from "./components/Group";
 
+import { ProjectContext } from "./utils/Contexts";
+
 import content from "./data/content";
 
-export default function Home() {
-  const modalRef = useRef();
+const Main = () => {
+  const modalRef = useRef<any | null>(null);
 
-  const [currentProject, setCurrentProject] = useState();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [currentProject, setCurrentProject] = useState<any | null>(null);
 
-  const handleDialogVisibility = (action) =>
+  const handleDialogVisibility = (action: string) =>
     action === "show" ? modalRef.current.showModal() : modalRef.current.close();
 
-  const handleSelectProject = (selectedProject) => {
+  const handleSelectProject = (selectedProject: any) => {
     setCurrentProject(selectedProject);
     handleDialogVisibility("show");
   };
 
+  const states = {
+    isModalOpen,
+    setIsModalOpen,
+    currentProject,
+    setCurrentProject,
+  };
+
   return (
-    <div className="wrapper">
-      <div className="overlay">
-        <header></header>
+    <ProjectContext.Provider value={states as any}>
+      <dialog open={isModalOpen} ref={modalRef}>
+        {currentProject ? (
+          <Modal
+            project={currentProject}
+            handleDialogVisibility={handleDialogVisibility}
+          />
+        ) : null}
+      </dialog>
 
-        <dialog ref={modalRef}>
-          {currentProject ? (
-            <Modal
-              project={currentProject}
-              handleDialogVisibility={handleDialogVisibility}
-            />
-          ) : null}
-        </dialog>
+      <main>
+        <section>
+          <Group
+            id="vanilla"
+            heading="HTML CSS JavaScript"
+            projects={content.vanilla}
+            handleSelectProject={handleSelectProject}
+          />
+        </section>
 
-        <main>
-          <section>
-            <Group
-              id="vanilla"
-              heading="HTML CSS JavaScript"
-              projects={content.vanilla}
-              handleSelectProject={handleSelectProject}
-            />
-          </section>
+        <section>
+          <Group
+            id="design"
+            heading="Design briefs | Repsonsive design"
+            projects={content.briefs}
+            handleSelectProject={handleSelectProject}
+          />
+        </section>
 
-          <section>
-            <Group
-              id="design"
-              heading="Design briefs | Repsonsive design"
-              projects={content.briefs}
-              handleSelectProject={handleSelectProject}
-            />
-          </section>
+        <section>
+          <Group
+            id="react"
+            heading="React"
+            projects={content.react}
+            handleSelectProject={handleSelectProject}
+          />
+        </section>
 
-          <section>
-            <Group
-              id="react"
-              heading="React"
-              projects={content.react}
-              handleSelectProject={handleSelectProject}
-            />
-          </section>
-
-          <section>
-            <Group
-              id="threejs"
-              heading="ThreeJS"
-              projects={[...new Array(1)].map((v, i) => content.projects[i])}
-              handleSelectProject={handleSelectProject}
-            />
-          </section>
-        </main>
-
-        <footer></footer>
-      </div>
-    </div>
+        <section>
+          <Group
+            id="threejs"
+            heading="ThreeJS"
+            projects={[...new Array(1)].map((v, i) => content.projects[i])}
+            handleSelectProject={handleSelectProject}
+          />
+        </section>
+      </main>
+    </ProjectContext.Provider>
   );
-}
+};
+
+export default Main;
