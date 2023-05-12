a92365a4792a8cb865d305a8d0e8079636c142be
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-12 17:33:31 +0100

    Implement Server/Client Component pattern: pass SC to CC as props children

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,57 @@
 // export const revalidate = 0;
-import Main from "./Main";
+
+// Types
+import { IProject } from "./types/application";
+
+// Data
+import content from "./data/content";
+
+// Components
+import Main from "./Main"; // client
+import Group from "./components/Group"; // server
 
 export default function Home() {
   return (
     <div className="wrapper">
       <div className="overlay">
         <header></header>
-        <Main />
+        <Main>
+          <section>
+            <Group
+              id="vanilla"
+              heading="HTML CSS JavaScript"
+              projects={content.vanilla as IProject[]}
+            />
+          </section>
+
+          <section>
+            <Group
+              id="design"
+              heading="Design briefs | Repsonsive design"
+              projects={content.briefs as IProject[]}
+            />
+          </section>
+
+          <section>
+            <Group
+              id="react"
+              heading="React"
+              projects={content.react as IProject[]}
+            />
+          </section>
+
+          <section>
+            <Group
+              id="threejs"
+              heading="ThreeJS"
+              projects={
+                [...new Array(1)].map(
+                  (v, i) => content.projects[i]
+                ) as IProject[]
+              }
+            />
+          </section>
+        </Main>
         <footer></footer>
       </div>
     </div>
