93cd0bd99d7bba31e10cac8872610ca80b9202df
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-15 16:11:16 +0100

    Add div className; Fix typo

--- app/components/Git/Repository.tsx
+++ app/components/Git/Repository.tsx
@@ -1,4 +1,4 @@
-import "server only";
+import "server-only";
 
 import React from "react";
 import Contents from "./Contents";
@@ -21,6 +21,7 @@ const Repository = async (props: Props) => {
 
   return (
     <div
+      className="repo-drawer"
       style={{
         top: "11rem",
         width: "100%",
