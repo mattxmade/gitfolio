d006f73892221c5e835ac2377e8431656516edde
Matt <97047080+mattxmade@users.noreply.github.com>
2023-05-18 13:44:16 +0100

    Add type safety; create new varibale and cast to type; add comments

--- app/ui/nameTag.ts
+++ app/ui/nameTag.ts
@@ -1,21 +1,31 @@
 const nameTag = (() => {
-  const create = (iconName, e) => {
+  const create = (
+    iconName: string,
+    e: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    // cast mouse event target
+    const parentElement = e.target as HTMLElement;
+
+    // remove any existing name tags from DOM
     document.querySelector(".name-tag")?.remove();
 
+    // create name tag
     const pElement = document.createElement("p");
     pElement.className = "name-tag";
     pElement.classList.add("name-tag-" + iconName.toLowerCase());
     pElement.textContent = iconName;
 
+    // add styling
     pElement.style.color = "whitesmoke";
     pElement.style.backgroundColor = "darkslategrey";
 
-    e.target.appendChild(pElement);
+    // append to parent element
+    parentElement.appendChild(pElement);
   };
 
   const remove = () => {
     const activeNameTag = document.querySelector(".name-tag");
-    activeNameTag.remove();
+    activeNameTag?.remove();
   };
 
   return {
