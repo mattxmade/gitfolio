4e4a065308b48f0f2baea93e4b247cff585ce215
Matt <97047080+mattxmade@users.noreply.github.com>
2023-06-04 12:34:11 +0100

    Restructure elements; add dynamic imports; update await function call

--- app/components/Git/Repository.tsx
+++ app/components/Git/Repository.tsx
@@ -1,52 +1,46 @@
 import "server-only";
+import dynamic from "next/dynamic";
 
-import React from "react";
-import Contents from "./Contents";
+const Contents = dynamic(() => import("./Contents"));
 
 import github from "@/api/services/github/github";
 import { IRepository, IGitResponse } from "@/app/types/application";
 
 type Props = {
+  id: string;
   repoName: string;
+  requireCommits: boolean;
   children?: React.ReactNode;
 };
 
 const { handleRepoContentRequest } = github;
 
-const Repository = async (props: Props) => {
-  const request = (await handleRepoContentRequest(props.repoName)) as Response;
+const Repository = async ({ id, repoName, requireCommits }: Props) => {
+  const request = (await handleRepoContentRequest(
+    repoName,
+    requireCommits
+  )) as Response;
   const response = (await request.json()) as IGitResponse;
 
   const gitRepo = response.data as IRepository;
 
   return (
     <div
-      className="repo-drawer"
+      id={"repo-folder-" + id}
+      className="repo-folder"
       style={{
-        top: "11rem",
-        width: "100%",
-        height: "calc(100% - 11rem)",
+        zIndex: 1,
+        height: "100%",
         position: "absolute",
-        overflow: "hidden",
-        padding: "0.5rem 0 0",
+        backgroundColor: "darkslategrey",
+        borderRadius: "0.2rem",
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        boxShadow: "0 0 0.5rem 0.1rem darkslategrey",
+        overflowY: "scroll",
       }}
     >
-      <div
-        className="repo-folder"
-        style={{
-          zIndex: 1,
-          height: "100%",
-          position: "absolute",
-          backgroundColor: "darkslategrey",
-          borderRadius: "0.2rem",
-          borderBottomLeftRadius: 0,
-          borderBottomRightRadius: 0,
-          boxShadow: "0 0 0.5rem 0.1rem darkslategrey",
-          overflowY: "scroll",
-        }}
-      >
-        <Contents name={gitRepo.name} contents={gitRepo.contents} />
-      </div>
+      <Contents id={id} contents={gitRepo.contents} />
     </div>
   );
 };
