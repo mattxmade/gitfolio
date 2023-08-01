4fb8641467db3bbcee9f93f60909a1158567b991
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-01 16:06:46 +0100

    Create new types. Update existing type

--- app/types/application.ts
+++ app/types/application.ts
@@ -1,8 +1,9 @@
 import { StaticImageData } from "next/image";
+import { DirectoryItem } from "@/app/api/services/github/types";
 
 export type IProject = {
-  id?: string;
-  repo?: string;
+  id: string;
+  repo: string;
   title: string;
   tech: Array<string>;
   description: string;
@@ -11,3 +12,14 @@ export type IProject = {
   img: { src: StaticImageData; alt: string };
   url: { live?: string; github?: string };
 } | null;
+
+export type IGitResponse = {
+  status: string;
+  message: string;
+  data: IRepository;
+};
+
+export type IRepository = {
+  name: string;
+  contents: DirectoryItem[];
+};
