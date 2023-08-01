2d8b97c2778187c5146e158f798fe54375b99c4f
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-01 16:51:25 +0100

    Add Component render conditions; Handle logic to display file commit history on button press

--- app/components/Git/FileModal.tsx
+++ app/components/Git/FileModal.tsx
@@ -1,7 +1,71 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 
 import FileViewer from "./FileViewer";
-import { DirectoryItem } from "@/api/services/github/types";
+
+import {
+  App_PatchItem,
+  App_SubmoduleItem,
+} from "@/app/api/services/github/types";
+
+const editPatch = (patch: string) =>
+  patch.slice(patch.length - 25) === "No newline at end of file"
+    ? patch.slice(0, patch.length - 27)
+    : patch;
+
+// App_PatchItem should be App_CommitItem
+const commitFactory = (commit: App_PatchItem) => {
+  let patch = editPatch(commit.patch);
+  let title: string | undefined;
+
+  // Patch changes format: @@ -0,0 +1 @@
+  const onlyAtAts: Array<{ inserts: string; index: number }> = []; //
+
+  patch.split("").map((char, i) => {
+    if (onlyAtAts.length === 4) return;
+    if (char === "@") onlyAtAts.push({ inserts: char, index: i });
+  });
+
+  if (onlyAtAts.length) {
+    const insertStart = onlyAtAts[0].index; // >@@ ... @@
+    const insertEnd = onlyAtAts[3].index; //..  @@ ... @@<
+
+    // Patch Title @@ ... @@ | get inserts also
+    title = patch.slice(insertStart, insertEnd + 1);
+
+    // Remove patch insert @@ ... @@ | only want code
+    commit.patch = patch.slice(insertEnd + 2);
+  } else commit.patch = patch;
+
+  // additions | deletions
+  const adds: Array<{ line: string; index: number }> = [];
+  const dels: Array<{ line: string; index: number }> = [];
+
+  const newLine = new RegExp("\n");
+
+  // single changes array | changes.line.index : changes.line.decoration
+  patch
+    .split(newLine)
+    .map((line, i) =>
+      line.startsWith("+") ? adds.push({ line, index: i }) : null
+    );
+  patch
+    .split(newLine)
+    .map((line, i) =>
+      line.startsWith("-") ? dels.push({ line, index: i }) : null
+    );
+
+  console.log("Additions: " + adds.length);
+  console.log("Deletions: " + dels.length);
+
+  return { title, adds, dels };
+};
+
+type Modified_CommitItem = {
+  commit: App_PatchItem;
+  title: string | undefined;
+  adds: Array<{ inserts: string; index: number }> | [];
+  dels: Array<{ inserts: string; index: number }> | [];
+};
 
 type Dialog = React.DetailedHTMLProps<
   React.DialogHTMLAttributes<HTMLDialogElement>,
@@ -9,8 +73,9 @@ type Dialog = React.DetailedHTMLProps<
 >;
 
 type Props = {
-  file: DirectoryItem;
+  source: App_SubmoduleItem & App_PatchItem;
   children?: React.ReactNode;
+  closeFileViewer: () => void;
 } & Dialog;
 
 const styles = {
@@ -21,10 +86,35 @@ const styles = {
   },
 };
 
-const FileModal = ({ file, children, open, ...props }: Props) => {
-  if (file.type === "dir" || file.type === "symlink") return <Fragment />;
+type FileSource = App_SubmoduleItem & App_PatchItem;
 
+const FileModal = ({
+  open,
+  source,
+  children,
+  closeFileViewer,
+  ...props
+}: Props) => {
   const REF_FileModal = useRef<HTMLDialogElement | null>(null);
+
+  const file =
+    source.type === "file" ? ({ ...source } as App_SubmoduleItem) : null;
+
+  // FIX TYPES
+  // Incorrect type App_PatchItem => App_CommitItem
+  const commit =
+    source.type === "patch"
+      ? { ...source, ...commitFactory({ ...source }) }
+      : null;
+
+  const filename = (file && file.name) || (commit && commit.filename);
+
+  const cmon = commit ? { ...source } : null;
+  if (cmon) cmon.patch = editPatch(cmon?.patch);
+
+  const codeFile = (file && file) || (commit && cmon);
+
+  // handleViewFile
 
   useEffect(() => {
     open && REF_FileModal.current
@@ -36,12 +126,48 @@ const FileModal = ({ file, children, open, ...props }: Props) => {
     <dialog ref={REF_FileModal} {...props}>
       <div className="modal--repo-file__overlay">
         <div className="modal--repo-file__header" style={styles.header}>
-          <h2>{file ? file.name || file.filename : ""}</h2>
-          <div style={{ display: "flex" }}>{children}</div>
-        </div>
+          {filename ? <h2>{filename}</h2> : null}
 
-        <FileViewer file={file} />
+          <div style={{ display: "flex" }}>
+            {commit ? (
+              <button
+                className="code-view__history-btn"
+                style={{
+                  gap: "1rem",
+                  display: "flex",
+                  alignItems: "center",
+                  margin: "1rem 2rem 1rem 1rem",
+                  position: "relative",
+                  zIndex: 2,
+                }}
+                onClick={() => {
+                  document
+                    .querySelector(".code-view__aside")
+                    ?.classList.toggle("hide");
+                }}
+              >
+                <i className="fa fa-solid fa-clock-rotate-left" />{" "}
+                <p>history</p>
+              </button>
+            ) : null}
+
+            <button
+              className="modal__close-button"
+              style={{
+                color: "whitesmoke",
+                position: "relative",
+                zIndex: 2,
+              }}
+              onClick={closeFileViewer}
+            >
+              <i className="fa fa-solid fa-circle-xmark" />
+            </button>
+          </div>
+        </div>
       </div>
+
+      {source ? <FileViewer source={codeFile as FileSource} /> : null}
+      {children}
     </dialog>
   );
 };
