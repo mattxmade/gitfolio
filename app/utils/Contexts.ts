a3dd8cabbc1966866dc2c990c0e30b66a237dcab
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-01 16:09:45 +0100

    Add new key/value to ContextProvider

--- app/utils/Contexts.ts
+++ app/utils/Contexts.ts
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import { IProject } from "../types/application";
+import { DirectoryItem } from "@/app/api/services/github/types";
 
 // TODO
 // - [x] Create Project Type
@@ -9,10 +10,14 @@ import { IProject } from "../types/application";
 // Need to set types using Dispatach + SetStateAction with expected type
 
 export type IProjectContextProvider = {
+  isHydrated: React.MutableRefObject<boolean>;
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
   currentProject: IProject;
   setCurrentProject: React.Dispatch<React.SetStateAction<any>>;
+  currentProjects: React.MutableRefObject<
+    Array<{ id: string; hasData: boolean; ui: boolean }>
+  >;
 } | null;
 
 export const ProjectContext = createContext<IProjectContextProvider>(null);
