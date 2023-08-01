d56807e27642890abd9c95acb916191ab735a3e1
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-01 13:15:42 +0100

    Add DivMask loading container; improve types; disable image button

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -1,9 +1,7 @@
 // server component
+import "server-only";
 
-import React, { Fragment, Suspense } from "react";
-
-// next Components
-import Link from "next/link";
+import { Fragment } from "react";
 import Image from "next/image";
 
 // Custom Components
@@ -11,75 +9,99 @@ import Icon from "./core/Icon";
 import Button from "./widgets/Button";
 import NameTag from "./widgets/NameTag";
 import FolderBtn from "./Git/FolderBtn";
-import Repository from "./Git/Repository";
+
+import DivMask from "./transitions/elementLibrary";
+import LoadingSpinner from "./widgets/LoadingSpinner";
 
 // Application Types
 import { IProject } from "../types/application";
 
-type Props = { project: IProject };
+type ProjectProps = {
+  index: number;
+  children?: React.ReactNode;
+  project: IProject;
+};
 
-const Project = ({ project }: React.PropsWithChildren<Props>) => {
+const Project = ({ project, ...props }: ProjectProps) => {
   if (!project) return <Fragment />;
 
   return (
     <Fragment>
-      <Suspense>
-        {/* @ts-expect-error [2]*/}
-        <Repository repoName={project.repo} />
-      </Suspense>
+      <div id={`${project.id}-repo-drawer`} className="repo-drawer" />
 
       <aside className="project-card__aside">
-        <h2>{project.title}</h2>
+        <DivMask offset={props.index + 1} delay={200}>
+          <h2>{project.title}</h2>
+        </DivMask>
 
-        <p className="project-card__description project-card-focus">{`${project.description}`}</p>
+        <DivMask offset={props.index + 1} delay={200}>
+          <p className="project-card__description project-card-focus">{`${project.description}`}</p>
+        </DivMask>
 
-        <div className="project-card-focus">
-          <h3>Features</h3>
-          <ul className="project-card__feature-list">
-            {project.features.map((feature, i) => (
-              <li key={"project-features_" + i}>
-                <p>{feature}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <DivMask offset={props.index + 1} delay={200}>
+          <div className="project-card-focus">
+            <h3>Features</h3>
+            <ul className="project-card__feature-list">
+              {project.features.map((feature, i) => (
+                <li key={"project-features_" + i}>
+                  <p>{feature}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </DivMask>
+        {props.children}
 
-        <div className="project-card__tech">
-          <h3>Technologies</h3>
+        <DivMask offset={props.index + 1} delay={200}>
+          <div className="project-card__tech">
+            <h3>Technologies</h3>
 
-          <ul className="project-card__tech-icons-list">
-            {project.tech.map((name, i) => (
-              <li key={"project-tech_" + i}>
-                <Icon name={name} />
-                <NameTag name={name} className="project-card__icon-mask" />
-              </li>
-            ))}
-          </ul>
-        </div>
+            <ul className="project-card__tech-icons-list">
+              {project.tech.map((name, i) => (
+                <li key={"project-tech_" + i}>
+                  <Icon name={name} />
+                  <NameTag name={name} className="project-card__icon-mask" />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </DivMask>
 
         <div className="project-card__icons project-card-focus">
-          <a href={project.url.live} target="_blank" rel="noreferrer">
+          <a
+            href={project.url.live || "#"}
+            target="_blank"
+            rel="noreferrer"
+            aria-label={`${project.title} live link`}
+          >
             <i className="fa fa-solid fa-globe" />
           </a>
-          <a href={project.url.github} target="_blank" rel="noreferrer">
+          <a
+            href={project.url.github || "#"}
+            target="_blank"
+            rel="noreferrer"
+            aria-label={`${project.title} git repo link`}
+          >
             <i className="fa fa-brands fa-github" />
           </a>
-          <FolderBtn />
+          <LoadingSpinner>
+            <FolderBtn id={project.id} />
+          </LoadingSpinner>
         </div>
       </aside>
 
-      <div className="project-card__image-container">
-        {/* <Link href={`/projects/${project.title}`}> */}
-        <Image
-          priority // [1]
-          id={`project-card__image-${project.id}`}
-          src={project.img.src}
-          alt={project.img.alt}
-        />
-        <Button project={project} className="img-mask">
+      <div className={"project-card__image-container"}>
+        <DivMask offset={props.index + 1} delay={200}>
+          <Image
+            priority // [1]
+            id={`project-card__image-${project.id}`}
+            src={project.img.src}
+            alt={project.img.alt}
+          />
+        </DivMask>
+        {/* <Button project={project} className="img-mask">
           <i className="fa fa-solid fa-maximize" />
-        </Button>
-        {/* </Link> */}
+        </Button> */}
       </div>
     </Fragment>
   );
@@ -111,5 +133,3 @@ export default Project;
  * Fixed in TypeScript 5.1+
  * =============================================================================
  */
-
-//
