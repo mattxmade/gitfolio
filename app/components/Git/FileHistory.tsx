aa4c11c972e541bf8dc703b3bfb734ffa9b48e3a
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-01 13:27:34 +0100

    Build upon Component: handle commitHistory; render elements of aside; add types

--- app/components/Git/FileHistory.tsx
+++ app/components/Git/FileHistory.tsx
@@ -1,18 +1,78 @@
 import React, { Fragment, useState } from "react";
-import { App_CommitItem, ICommitItem } from "@/api/services/github/types";
+import {
+  IPatchItem,
+  ICommitItem,
+  App_PatchItem,
+  App_CommitItem,
+  App_SubmoduleItem,
+} from "@/app/api/services/github/types";
+
+import { sortCommitsByDate } from "@/app/utils/processCommits";
+
+type HandleViewFile = (
+  file: App_SubmoduleItem | App_PatchItem,
+  commits: App_CommitItem[] | null
+) => void;
 
 type Props = {
-  commits: Array<ICommitItem | App_CommitItem>;
+  commits: Array<App_CommitItem>;
   children?: React.ReactNode;
+  handleViewFile: HandleViewFile;
 };
 
-const FileHistory = ({ commits, children }: Props) => {
+const FileHistory = ({ commits, children, handleViewFile }: Props) => {
+  const commitHistory = sortCommitsByDate(commits);
+
   return (
-    <aside style={{ position: "absolute" }}>
-      {commits.map((item, index) => {
-        return <Fragment />;
-      })}
-    </aside>
+    <Fragment>
+      <aside
+        className={"code-view__aside hide"}
+        style={{ position: "absolute" }}
+      >
+        {commitHistory.map((history, index) => {
+          return (
+            <ul key={history.date + "_" + index}>
+              <li className="commit-start commit-divider">
+                <p>
+                  <span>
+                    <i className="fa fa-solid fa-code-commit" />
+                  </span>
+                  {`Commits on ${history.date}`}
+                </p>
+              </li>
+
+              {history.commits.byDate.map((commit, i) => (
+                <li key={commit.sha}>
+                  <div className="code-view__aside__content">
+                    <h4
+                      onClick={
+                        history.commits.all.length > 1
+                          ? () => {
+                              handleViewFile(commit.file, history.commits.all);
+                            }
+                          : () => null
+                      }
+                    >
+                      {commit.message}
+                    </h4>
+                    <p>{`${commit.author.name} commited on ${history.date}`}</p>
+                    <div className="commit-stem"></div>
+                  </div>
+                </li>
+              ))}
+              <li className="commit-end commit-divider">
+                <p>
+                  <span>
+                    <i className="fa fa-solid fa-code-commit" />
+                  </span>
+                  End of commit history for this file
+                </p>
+              </li>
+            </ul>
+          );
+        })}
+      </aside>
+    </Fragment>
   );
 };
 
