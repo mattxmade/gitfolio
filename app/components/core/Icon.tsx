c16300d0cb3a7d61b5eec2496bb796d71ffe953c
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-17 18:51:11 +0100

    Update Icon.tsx: Add icons to switch/case

--- app/components/core/Icon.tsx
+++ app/components/core/Icon.tsx
@@ -14,10 +14,22 @@ const Icon = (props: Props) => {
       return <i className="tech-icon fa-brands fa-html5" />;
     case "CSS":
       return <i className="tech-icon fa-brands fa-css3-alt" />;
+    case "Git":
+      return <i className="fa-brands fa-git" />;
     case "JavaScript":
       return <i className="tech-icon fa-brands fa-square-js" />;
+    case "Laravel":
+      return <i className="fa-brands fa-laravel" />;
+    case "Node":
+      return <i className="fa-brands fa-node" />;
+    case "NPM":
+      return <i className="fa-brands fa-npm" />;
+    case "PHP":
+      return <i className="fa-brands fa-php" />;
     case "React":
       return <i className="tech-icon fa-brands fa-react" />;
+    case "Sass":
+      return <i className="fa-brands fa-sass" />;
     case "ThreeJS":
       return <i className="tech-icon fa-solid fa-cube" />;
     case "Webpack":
