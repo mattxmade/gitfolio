89f05a112af21b8b44e8edd4f58077cf9b51bf24
Matt <97047080+mattxmade@users.noreply.github.com>
2024-11-10 21:49:23 +0000

    Configure next  metadata; initial commit

---
+++ app/data/metadata.ts
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export const projectMetadata: Metadata = {
+  title: "Gifolio",
+  description:
+    "Git powered Web developer portfolio " + new Date().getFullYear(),
+  robots: {},
+};
